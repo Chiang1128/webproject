3475149028389dcd455587eae8f4e3c19fc52980
Chiang1128 <124015003+Chiang1128@users.noreply.github.com>
2024-12-24 01:50:33 +0800

    Delete city.js

--- city.js
+++
@@ -1,8 +0,0 @@
-const urlParams = new URLSearchParams(window.location.search);
-const title = urlParams.get("title");
-const description = urlParams.get("description");
-const image = urlParams.get("image");
-
-document.getElementById("spot-title").innerText = title;
-document.getElementById("spot-description").innerText = description;
-document.getElementById("spot-image").src = image;
